3aecc626c486b48e1ff0e83b2ac3ba2f8f62631d
Ahmed Alaa <ahmedalaa1316@gmail.com>
2023-05-04 00:43:09 +0300

    the first form

--- django_project/ElectricityBills/models.py
+++ django_project/ElectricityBills/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    codenumber = models.CharField(max_length=20)
+    meterreding = models.CharField(max_length=20)
+    company = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images/')
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.number
--- django_project/ElectricityBills/views.py
+++ django_project/ElectricityBills/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    codenumber = request.POST.get("number")
+    meterreding = request.POST.get("meterreder")
+    company = request.POST.get("address")
+    image = request.POST.get("image")
+    comments = request.POST.get("comment")
+    return render(request,'ElectricityBills/form1.html')
