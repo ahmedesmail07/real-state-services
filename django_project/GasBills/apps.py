9e61099a953cf3f30d4536ebd316f2d3210cc7b4
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-05-03 20:36:25 +0300

    Configure Apps

---
+++ django_project/GasBills/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GasbillsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "GasBills"
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,8 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
-        "APP_DIRS": True,
+        "APP_DIRS": False,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -66,6 +67,12 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 WSGI_APPLICATION = "django_project.wsgi.application"
 
@@ -115,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
